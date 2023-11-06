1db52177e5c25806d719a1194727394029b769e7
Irina Garmaeva <garmaeva-i@mail.ru>
2023-11-06 18:13:06 +0100

    Initialize repo, add OrderBook component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import OrderBook from './components/OrderBook/OrderBook';
+
+
+function App() {
+
+
+  return <div className='root'>
+    <h2>Crypto Order Book </h2>
+    <OrderBook/>
+  </div>;
+}
+
+export default App;
